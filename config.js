dbbbc04616e7cd13f2f0c689b0aa0f67bd37a754
joynicolas <34314171+joynicolas@users.noreply.github.com>
2026-05-07 18:49:35 +0530

    Update config.js
    removed gemini key

--- config.js
+++ config.js
@@ -3,4 +3,3 @@
 
 window.SUPABASE_URL = "https://hmfqxufsppbwynaeenaw.supabase.co";
 window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhtZnF4dWZzcHBid3luYWVlbmF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxNDA2ODYsImV4cCI6MjA5MzcxNjY4Nn0.N1XYJDdmQum07KRto3TBXxxevMZ3fuQzb9AilebIRQ8";
-window.GEMINI_API_KEY = "AIzaSyD9xRFA8VxvufF7LeX0JFKnmZ6iKox72eI";
